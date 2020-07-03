26837623b31cf389764ff8fdae65f5bb2aa0fcf4
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-03 15:52:43 -0600

    Update script

--- scripts/change_pane.sh
+++ scripts/change_pane.sh
@@ -1,27 +1,28 @@
-#!/bin/sh
-PROC_NAME=$(osascript -e "
-  tell application \"iTerm\"
-    activate
-    tell the current terminal
-      tell the current session
-        get name
-      end tell
+#!/bin/bash
+PROCNAME=$(osascript -e "
+    tell application \"iTerm2\"
+        activate
+        tell current window
+            tell the current session
+                get name
+            end tell
+        end tell
     end tell
-end tell")
+")
 if [[ "$PROC_NAME" == *"vim"* ]];
 then
     echo '\x1b'
     echo ":call iTerm2AwareNavigate(\"$1\")"
     echo '\r'
 else
-    if $1 == 'h' then
-        keycode = '123'
-    else if $1 == 'l'
-        keycode = 124
-    else if $1 == 'j'
-        keycode = 125
-    else if $1 == 'k'
-        keycode = 126
-    if
+    if [[ "$1" == "h" ]]; then
+        keycode='123'
+    elif [[ "$1" == "l" ]]; then
+        keycode=124
+    elif [[ "$1" == "j" ]]; then
+        keycode=125
+    elif [[ "$1" == "k" ]]; then
+        keycode=126
+    fi
     osascript -e "tell application \"System Events\" to key code $keycode using {command down, option down}"
 fi
